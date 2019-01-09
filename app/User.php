9e4c963d96de0e979ef362be4500f62f893db228
Andrea Damayanti <andr.dmynti22@gmail.com>
2019-01-09 16:15:17 +0700

    add user

--- app/User.php
+++ app/User.php
@@ -5,9 +5,13 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    
+    use HasRoles;
+    
     use Notifiable;
 
     /**
@@ -15,6 +19,8 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $guard_name = 'web';
+    
     protected $fillable = [
         'name', 'email', 'password',
     ];
